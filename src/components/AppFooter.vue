52775fb6c895eca3ee3e4801df974791fb0d95c7
Kohxax <koharen0256@gmail.com>
2024-12-14 01:16:21 +0900

    powered byの記述追加

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,11 +1,37 @@
 <template>
-
+    <v-footer app class="bg-background text-white">
+        <v-row>
+            <v-col class="mx-auto text-center d-flex flex-column" cols="12">
+                <p>Powered by</p>
+                <div class="d-flex justify-center">
+                    <v-icon class="mx-2 d-flex" v-for="item in Powered" :size="item.size" :icon="item.icon"
+                        :color="item.color" :title="item.title"></v-icon>
+                </div>
+            </v-col>
+        </v-row>
+    </v-footer>
 </template>
 
 <script setup lang="ts">
+import CloudflareIcon from '@/components/icons/CloudflareIcon.vue';
+import UbuntuIcon from '@/components/icons/UbuntuIcon.vue';
 
+const Powered = [
+    {
+        title: 'Cloudflare',
+        icon: CloudflareIcon,
+        size: 40
+    },
+    {
+        title: 'Vuetify',
+        icon: '$vuetify',
+        size: 35,
+        color: "info"
+    },
+    {
+        title: 'Ubuntu',
+        icon: UbuntuIcon,
+        size: 32
+    }
+]
 </script>
-
-<style scoped lang="sass">
-
-</style>
