b09ac50997a380570a20fc6210fffab8ad0e62af
Kohxax <koharen0256@gmail.com>
2024-12-19 21:01:15 +0900

    main二つになってたの修正

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,28 +1,26 @@
 <template>
-  <v-main class="mt-40">
-    <v-container>
-      <v-row class="justify-center">
-        <v-col cols="12" md="12" lg="8" xl="6">
-          <v-sheet class="d-flex flex-column align-center rounded-lg" :elevation="4" min-height="150vh" max-width="1000"
-            rounded>
-            <h1>Blog Post here(WIP)</h1>
-          </v-sheet>
-        </v-col>
+  <v-container>
+    <v-row class="justify-center">
+      <v-col cols="12" md="12" lg="8" xl="6">
+        <v-sheet class="d-flex flex-column text-center rounded-lg" :elevation="4" min-height="150vh" max-width="1000"
+          rounded>
+          <h1>Blog Post here(WIP)</h1>
+        </v-sheet>
+      </v-col>
 
-        <v-col cols="12" lg="4" xl="3" class="order-last order-md-last">
-          <v-sheet class="d-flex flex-column align-center rounded-lg" :elevation="4" rounded>
-            <v-avatar class="mt-2" size="200" image="@/assets/bokukoha_icon.jpg"></v-avatar>
-            <div class="mx-5 my-3 text-center">
-              <p class="text-h5 font-weight-bold">こは / Koha</p>
-              <p class="mt-3">自宅サーバー初心者です。</p>
-              <p class="mt-3">Java, Vue, Ubuntuあたりをゆるーく触ってます。</p>
-              <p class="mt-3">PCゲーム、エロゲー、散歩が好きです。</p>
-            </div>
-          </v-sheet>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-main>
+      <v-col cols="12" lg="4" xl="3" class="order-last order-md-last">
+        <v-sheet class="d-flex flex-column align-center rounded-lg" :elevation="4" rounded>
+          <v-avatar class="mt-2" size="200" image="@/assets/bokukoha_icon.jpg"></v-avatar>
+          <div class="mx-5 my-3 text-center">
+            <p class="text-h5 font-weight-bold">こは / Koha</p>
+            <p class="mt-3">自宅サーバー初心者です。</p>
+            <p class="mt-3">Java, Vue, Ubuntuあたりをゆるーく触ってます。</p>
+            <p class="mt-3">PCゲーム、エロゲー、散歩が好きです。</p>
+          </div>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
@@ -54,11 +52,9 @@ const links = [
 </script>
 
 <style lang="css" scoped>
-.mt-40 {
-  margin-top: 40px;
-}
-
-h1, p, v-avatar {
+h1,
+p,
+v-avatar {
   user-select: none;
 }
 </style>
